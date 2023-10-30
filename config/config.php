859ce204343ca53f87d615276423631903d1c2bc
Gianluigi <a35g@hackworld.it>
2023-10-30 18:24:56 +0100

    Create config.php
    Add config file

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings["app_url"] = "https://localhost/MuSa/";
+
+$settings["app_video_mime"] = array(
+    "video/ogg",
+    "video/mp4",
+    "video/webm"
+);
+
+$settings["app_lang"] = "it-IT";
